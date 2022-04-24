2224ec1273498e6c38e1c951aa659f3c7025c389
Hyunseok Kim <75778561+kingmuraa@users.noreply.github.com>
2022-04-25 06:54:44 +0900

    220425 : movie App part one

--- React-App/react-for-beginners/src/App.js
+++ React-App/react-for-beginners/src/App.js
@@ -1,33 +1,52 @@
-import { useEffect, useState } from  "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([])
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
-      .then(response => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async() => {
+    const json = await (
+      await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=10&sort_by=year`
+      )
+    ).json()
+    setMovies(json.data.movies);
+    setLoading(false);
+    };
+  // const getMovies = async() => {
+  //   const response = await fetch(
+  //     `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+  //   )
+  //   const json = await response.json();
+  //   setMovies(json.data.movies);
+  //   setMovies(json.data.movies);
+  //   setLoading(false);
+  //   };
 
-  return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length}ea)`}</h1>
-      {loading ? ( 
-        <strong>Loading...</strong>
-      ) : (
-        <select>
-          {coins.map((coin) => (
-            <option>
-              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-            </option>
+  useEffect(() => {
+    getMovies();
+  }, []);
+  console.log(movies);
+  return ( 
+    <div> 
+    {loading ? (
+      <h1>Loading...</h1>
+    ) : (
+      <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>{movie.genres.map((g) => (
+                <li key={g}>{g}</li>
+              ))}
+              </ul>
+            </div>
           ))}
-       </select>
+        </div>
       )}
     </div>
   );
 }
 
-export default App;
+  export default App;
